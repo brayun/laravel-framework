4c164ce7994fcd1b16821698d3f2016073f582f1
Ethan <ethan@brayun.com>
2017-11-15 00:43:41 +0800

    init

---
+++ src/Module/Module.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ *          ┌─┐       ┌─┐
+ *       ┌──┘ ┴───────┘ ┴──┐
+ *       │                 │
+ *       │       ───       │
+ *       │  ─┬┘       └┬─  │
+ *       │                 │
+ *       │       ─┴─       │
+ *       └───┐         ┌───┘
+ *           │         └──────────────┐
+ *           │                        ├─┐
+ *           │                        ┌─┘
+ *           │                        │
+ *           └─┐  ┐  ┌───────┬──┐  ┌──┘
+ *             │ ─┤ ─┤       │ ─┤ ─┤
+ *             └──┴──┘       └──┴──┘
+ *  @Author Ethan <ethan@brayun.com>
+ */
+
+
+namespace Brayun\Foundation\Module;
+
+use Illuminate\Support\ServiceProvider;
+
+class Module extends ServiceProvider
+{
+
+}
---
+++ src/Routing/Abstracts/Controller.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ *          ┌─┐       ┌─┐
+ *       ┌──┘ ┴───────┘ ┴──┐
+ *       │                 │
+ *       │       ───       │
+ *       │  ─┬┘       └┬─  │
+ *       │                 │
+ *       │       ─┴─       │
+ *       └───┐         ┌───┘
+ *           │         └──────────────┐
+ *           │                        ├─┐
+ *           │                        ┌─┘
+ *           │                        │
+ *           └─┐  ┐  ┌───────┬──┐  ┌──┘
+ *             │ ─┤ ─┤       │ ─┤ ─┤
+ *             └──┴──┘       └──┴──┘
+ *  @Author Ethan <ethan@brayun.com>
+ */
+
+
+namespace Brayun\Foundation;
+
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+abstract class Controller extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests, Dispatchable;
+}
